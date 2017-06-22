1489bb3ee622b250b8fa8f2687c01f4816ddf0b7
tomz123 <tomz12321@gmail.com>
2017-06-23 00:23:38 +0800

    SetupAndFirstNodeJS
    Express, npm and yarn

---
+++ install_node.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Upgrade npm
+npm install -g npm
+
+# Use yarn instead of npm
+# Install react developing environment
+#npm install -g create-react-app
+yarn global add create-react-app
+#npm install -g webpack
+yarn global add webpack
+yarn global add cross-env
+yarn global add webpack-dev-server
+yarn global add eslint
+yarn global add editorconfig-tools
+yarn global add eslint-to-editorconfig
+yarn global add mocha
+yarn global add jest
+yarn global add gh-pages
+yarn global add bower
+yarn global add typescript
+yarn global add typings
